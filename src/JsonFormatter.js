e4a126ecd76b5ed124b9d3ff196fe8636129ffdd
David Furnes <david@dfurnes.com>
2017-02-21 02:59:26 -0500

    Better formatting! :art:

--- src/JsonFormatter.js
+++ src/JsonFormatter.js
@@ -4,6 +4,8 @@ const ora = require('ora');
 const prettyMs = require('pretty-ms');
 const print = console.log;
 const printJson = require('jsome');
+const logSymbols = require('log-symbols');
+const emphasize = require('emphasize');
 const { startTimer, endTimer } = require('./helpers');
 
 // json formatting
@@ -34,18 +36,26 @@ class JsonFormatter {
   }
 
   printResponse(response, ms) {
-    const successful = response.status >= 200 && response.status <= 300;
-    const statusColor = successful ? chalk.green : chalk.red;
-
-    let message = `${statusColor.bold(response.status)} ${statusColor(response.statusText)} `;
-    message += chalk.gray(`[${prettyMs(ms)}]`);
-    message += '\n';
-
-    if (successful) {
-      this.spinner.succeed(message);
+    let symbol, color;
+    if (response.status >= 200 && response.status < 300) {
+      symbol = logSymbols.success;
+      color = chalk.green;
+    } else if (response.status >= 400 && response.status < 500) {
+      symbol = logSymbols.warning;
+      color = chalk.yellow;
+    } else if (response.status >= 500) {
+      symbol = logSymbols.error;
+      color = chalk.red;
     } else {
-      this.spinner.fail(message);
+      symbol = chalk.cyan(process.platform === 'win32' ? 'i' : 'ℹ');
+      color = chalk.cyan;
     }
+
+    let text= `${color.bold(response.status)} ${color(response.statusText)} `;
+    text += chalk.gray(`[${prettyMs(ms)}]`);
+    text += '\n';
+
+    this.spinner.stopAndPersist({symbol, text});
   }
 
   printError(error, ms) {
@@ -65,7 +75,25 @@ class JsonFormatter {
   }
 
   printData(response) {
-      printJson(response.data);
+    if (! response.data) {
+      return print(chalk.gray('<empty response>'));
+    }
+
+    // Handle pretty-printing common types of content (by header).
+    if (response.headers && response.headers['content-type']) {
+      let type = response.headers['content-type'];
+
+      if (type.includes('application/json')) {
+        return printJson(response.data);
+      } else if (type.includes('text/html') || type.includes('application/xml')) {
+        return print(emphasize.highlight('xml', response.data).value);
+      } else if (type.includes('text/plain')) {
+        return print(response.data);
+      }
+    }
+
+    // ...or try to guess!
+    return print(emphasize.highlightAuto(response.data).value);
   }
 }
 
