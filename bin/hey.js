d782c7522a3f69145ca393ed89e0659fda4be5b6
David Furnes <david@dfurnes.com>
2017-02-22 23:34:31 -0500

    Accept all-caps HTTP methods as commands.

--- bin/hey.js
+++ bin/hey.js
@@ -4,10 +4,16 @@ const program = require('commander');
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
 
-
 // Notify the user if updates are available.
 const DAILY = 1000 * 60 * 60 * 24;
 updateNotifier({pkg, updateCheckInterval: DAILY}).notify();
+
+// Accept all-caps HTTP methods as commands.
+const methods = ['get', 'post', 'put', 'delete'];
+if (process.argv[2]) {
+  const method = process.argv[2].toLowerCase();
+  if (methods.includes(method)) process.argv[2] = method;
+}
 
 program
   .version(pkg.version)
