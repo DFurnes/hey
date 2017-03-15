51d2de8c7f56c95b260e4c001dc3a0cc1c42b5ab
David Furnes <david@dfurnes.com>
2017-03-15 19:42:02 -0400

    Add docblocks and break out request method.

--- src/Command.js
+++ src/Command.js
@@ -15,42 +15,57 @@ class Command {
       .option('-H, --header <values>', 'Set the request headers.', collectKeyValues, {})
       .option('-q, --query <values>', 'Set the request query-string.', collectKeyValues, {})
       .option('-v, --verbose', 'Increase the verbosity of the formatter.', increaseVerbosity, 0);
+
+    this.makeRequest = this.makeRequest.bind(this);
   }
 
+  /**
+   * Set an option on the command.
+   */
   set(key, value) {
     this.commandOptions[key] = value;
     return this;
   }
 
+  /**
+   * Set the command description.
+   */
   description(message) {
     this.program.description(message);
     return this;
   }
 
-  run() {
-    this.program
-      .usage(`<url> ${this.commandOptions['body'] ? '[data]' : '' }`);
+  /**
+   * Make the request.
+   */
+  makeRequest(url, data) {
+    let formatter = new DefaultFormatter(this.verbose);
 
-    this.program.action((url, data) => {
-      let formatter = new DefaultFormatter(this.verbose);
 
-      if (data && this.commandOptions['body']) {
-        const { headers, parsedData } = parseData(data);
-        this.commandOptions.headers = merge(headers, this.header),
+    if (data && this.commandOptions['body']) {
+      const { headers, parsedData } = parseData(data);
+      this.commandOptions.headers = merge(headers, this.header),
         this.commandOptions.data = parsedData;
-      }
+    }
 
-      const request = new Request(merge(this.commandOptions, {
-        url: url,
-        query: this.query,
-      }), formatter);
+    const request = new Request(merge(this.commandOptions, {
+      url: url,
+      query: this.query,
+    }), formatter);
 
-      request.send();
-    });
+    request.send();
+  }
+
+  /**
+   * Run the command.
+   */
+  run() {
+    this.program.usage(`<url> ${this.commandOptions['body'] ? '[data]' : '' }`);
+
+    this.program.action(this.makeRequest);
 
     this.program.parse(this.input);
   }
-
 }
 
 module.exports = Command;
